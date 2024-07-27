1884880ae719b7ecf52706981ac2053df6d3064a
vanshika2933 <122749140+vanshika2933@users.noreply.github.com>
2024-07-27 16:38:43 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,48 +1,25 @@
 from PIL import Image 
 
-# Create an image as input: 
-input_image = "C:\Users\HP\OneDrive\Pictures\1000101577.jpg" 
+input_image = "1000101577.jpg" 
 
-# save the image as "input.png" 
-#(not mandatory) 
-input_image.save("input", format="png") 
+input_image.save("input", format="jpg") 
 
-# Extracting pixel map: 
 pixel_map = input_image.load() 
 
-# Extracting the width and height 
-# of the image: 
 width, height = input_image.size 
 z = 100
 for i in range(width): 
 	for j in range(height): 
 		
-		# the following if part will create 
-		# a square with color orange 
 		if((i >= z and i <= width-z) and (j >= z and j <= height-z)): 
-			
-			# RGB value of orange. 
 			pixel_map[i, j] = (255, 165, 0) 
-
-		# the following else part will fill the 
-		# rest part with color light salmon. 
-		else: 
-			
-			# RGB value of light salmon. 
+		else:  
 			pixel_map[i, j] = (255, 160, 122) 
 
-# The following loop will create a cross 
-# of color blue. 
-for i in range(width): 
-	
-	# RGB value of Blue. 
+for i in range(width):  
 	pixel_map[i, i] = (0, 0, 255) 
 	pixel_map[i, width-i-1] = (0, 0, 255) 
 
-# Saving the final output 
-# as "output.png": 
-input_image.save("output", format="png") 
+input_image.save("output", format="jpg") 
 
-# use input_image.show() to see the image on the 
-# output screen. 
-input_image.show("output", format="png")
+input_image.show("output", format="jpg")
