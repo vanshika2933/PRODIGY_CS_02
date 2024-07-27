fdc60e5e99effb6e865fe857f933060be2403767
vanshika2933 <122749140+vanshika2933@users.noreply.github.com>
2024-07-27 16:49:00 +0530

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -2,30 +2,19 @@ from PIL import Image
 
 input_image = Image.open("1000101577.jpg") 
 
-# Extracting pixel map: 
 pixel_map = input_image.load() 
 
-# Extracting the width and height 
-# of the image: 
 width, height = input_image.size 
 
-# taking half of the width: 
 for i in range(width//2): 
 	for j in range(height): 
 		
-		# getting the RGB pixel value. 
 		r, g, b, p = input_image.getpixel((i, j)) 
 		
-		# Apply formula of grayscale: 
 		grayscale = (0.299*r + 0.587*g + 0.114*b) 
-
-		# setting the pixel value. 
+ 
 		pixel_map[i, j] = (int(grayscale), int(grayscale), int(grayscale)) 
 
-# Saving the final output 
-# as "grayscale.png": 
-input_image.save("grayscale", format="png") 
+input_image.save("grayscale", format="jpg") 
 
-# use input_image.show() to see the image on the 
-# output screen. 
-
+input_image.show()
